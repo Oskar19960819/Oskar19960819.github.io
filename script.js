e6ed8f9f135227350a917e9dc582ee50d09534b4
Oskar19960819 <116001486+Oskar19960819@users.noreply.github.com>
2022-10-24 12:57:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ button.onclick = function() {
     count +=1;
 };
 
+var nextStep = document.querySelector('#nextStep');
 
+nextStep.addEventListener('click', function (e) {
+  e.preventDefault();
+  // Hide first view
+  document.getElementById('my_form').style.display = 'none';
+
+  // Show thank you message element
+  document.getElementById('thank_you').style.display = 'block';
+});
